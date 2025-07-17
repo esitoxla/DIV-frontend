96d0bc503d1d67828f4099c9157eb4f68b481db1
Technologia2014 <eunicemaabenayayra@gmail.com>
2025-07-17 14:18:47 +0000

    sign branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import QrCodes from "./components/QrCodes";
+
 
 function App() {
   return (
     <>
-      <Hero />
-      <QrCodes />
-      <Footer />
+      
     </>
   );
 }
